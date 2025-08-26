73b5b4c232cd7244d5c4e87c251a0cc138e0f0e0
Benjamin Khong <ben@Mac.attlocal.net>
2025-08-25 22:07:48 -0700

    building a decentralized file storage system

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,30 @@
+package p2p
+
+import (
+	"net"
+	"sync"
+)
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+}
